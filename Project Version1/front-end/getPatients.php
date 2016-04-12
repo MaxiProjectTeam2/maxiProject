bdd0604cfb02ac505a876bba2cfb5fc0e180b943
HKClough1 <hannahclough@live.com>
2016-04-12 11:18:04 +0100

    Added files via upload

--- Project Version1/front-end/getPatients.php
+++ Project Version1/front-end/getPatients.php
@@ -1,29 +1,41 @@
 <?php
+
+class getPatients{
+  function getMatches($q){
+
+    require_once("database.php");
+
+    $findPatients = "SELECT forename,surname FROM patient ";
+    $findPatients .= "WHERE surname LIKE '%";
+    $findPatients .= $q;
+    $findPatients .= "%' ";
+    $findPatients .= "ORDER BY surname ASC";
+
+
+    // "resourse" objet - a collection of db rows
+    $result = $db->query($findPatients);
+    if(!$result){
+      die("Database query failed.");
+    }
+
+    while($patient = mysqli_fetch_assoc($result)){
+      $patients[] = array($patient["forename"],$patient["surname"]);
+    }
+
+    mysqli_free_result($result);
+
+    $db->close_connection();
+
+    return $patients;
+  }
+
+}
+  $set = new getPatients();
+
   $q = $_REQUEST["q"];
 
-  require_once("database.php");
-
-  $findPatients = "SELECT forename,surname FROM patient ";
-  $findPatients .= "WHERE surname LIKE '%";
-  $findPatients .= $q;
-  $findPatients .= "%' ";
-  $findPatients .= "ORDER BY surname ASC";
-
-
-  // "resourse" objet - a collection of db rows
-  $result = $db->query($findPatients);
-  if(!$result){
-    die("Database query failed.");
-  }
-
-  while($patient = mysqli_fetch_assoc($result)){
-    $patients[] = array($patient["forename"],$patient["surname"]);
-  }
-
-  mysqli_free_result($result);
+  $patients = $set->getMatches($q);
 
   echo json_encode($patients);
-
-  $db->close_connection();
 
 ?>
