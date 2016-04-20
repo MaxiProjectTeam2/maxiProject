ab37f427daa44b7ed5011bce34520cf194462e45
HKClough1 <hannahclough@live.com>
2016-04-20 10:20:44 +0100

    Added files via upload

--- trackResult.php
+++ trackResult.php
@@ -7,13 +7,10 @@
 	<body>
 		<h1>Track Result</h1>
 		<?php
-			$con = new mysqli("LocalHost","root","root","mydb");
-			if (!$con)
-  			{
-  				die('Could not connect: ' . mysql_error());
-  			}
-			$sql = "SELECT * FROM mydb.viewHistory";
-			$result = $con->query($sql);
+			require_once("database.php");
+
+			$sql = "SELECT * FROM rtooldb.viewHistory";
+			$result = $db->query($sql);
 			echo "<table>";
 			echo "<tr>";
 			echo "<th>Name</th>";
@@ -23,23 +20,25 @@
 			while($row = $result -> fetch_assoc()){
 				echo "<tr>";
 				$patientId = $row["patient_patientID"];
-				$nameSql = "SELECT forename as name FROM mydb.patient where patientId = " . $patientId;
+				$nameSql = "SELECT forename as name FROM rtooldb.patient where patientId = " . $patientId;
 				$patientname = $con->query($nameSql);
 				while($names = $patientname -> fetch_assoc()){
 					$name = $names["name"];
 					echo "<th>" . $name . "</th>";
 				}
-				$contentId = $row["content_contentID"];
-				$contentSql = "SELECT typeName FROM mydb.contentType where contentId =" . $contentId;
-				$contentName = $con->query($contentSql);
+				$contentId = $row["contentTypeID"];
+				$contentSql = "SELECT Type FROM rtooldb.contentType where contentTypeID =" . $contentId;
+				$contentName = $db->query($contentSql);
 				while($contents = $contentName->fetch_assoc()){
-					$cn = $contents["typeName"];
+					$cn = $contents["Type"];
 					echo "<th>" . $cn . "</th>";
 				}
 				$viewId = $row["ViewId"];
 				echo "<th>" . $viewId . "</th>";
 				echo "</tr>";
 			}
+
+			$db->close_connection();
 		?>
 	</body>
 </html>
